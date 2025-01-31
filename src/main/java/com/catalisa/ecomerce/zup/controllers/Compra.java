bd3798411849df00224de36e5285b2664580cae8
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-01-31 15:53:46 -0300

    feat(compra): adicionar classe Compra com atributos e métodos essenciais
    - Adicionados os atributos: cpfCliente, nomeProduto e quantidade.
    - Implementado o construtor para inicialização dos objetos.
    - Criados os métodos getters e setters para encapsulamento e controle de acesso.
    - Estrutura preparada para futuras validações e manipulações de dados relacionados às compras.

---
+++ src/main/java/com/catalisa/ecomerce/zup/controllers/Compra.java
@@ -0,0 +1,38 @@
+package com.catalisa.ecomerce.zup.controllers;
+
+public class Compra {
+    private String cpfCliente;
+    private String nomeProduto;
+    private int quantidade;
+
+    // Construtor, getters e setters
+    public Compra(String cpfCliente, String nomeProduto, int quantidade){
+        this.cpfCliente = cpfCliente;
+        this.nomeProduto = nomeProduto;
+        this.quantidade = quantidade;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
