9b9a5533df93215152854e768e8b9aeb237cb5a2
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-01-31 17:10:52 -0300

    feat(repository): implementar ClienteRepository com operações básicas de CRUD
    - Adicionado método getClientes() para listar todos os clientes.
    - Adicionado método findByCpf(String cpf) para buscar um cliente pelo CPF.
    - Adicionado método save(Cliente cliente) para salvar um novo cliente.
    - Estrutura inicial para gerenciar a persistência de clientes em memória

---
+++ src/main/java/com/catalisa/ecomerce/zup/repository/ClienteRepository.java
@@ -0,0 +1,26 @@
+package com.catalisa.ecomerce.zup.repository;
+import com.catalisa.ecomerce.zup.controllers.Cliente;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ClienteRepository {
+    private final List<Cliente> clientes = new ArrayList<>();
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public Optional<Cliente> findByCpf(String cpf) {
+        return clientes.stream()
+                .filter(c -> c.getCpf().equals(cpf))
+                .findFirst();
+    }
+
+    public void save (Cliente cliente) {
+        clientes.add(cliente);
+    }
+}
