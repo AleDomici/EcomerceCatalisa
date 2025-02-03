f9d585f60db8624cb4161aa0013e47016e5ee603
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-02-03 13:26:03 -0300

    feat(service): implementar ProdutoService com validações e operações CRUD
    - Adicionado o serviço ProdutoService para gerenciar operações relacionadas a produtos.
    - Implementado método listarProdutos para listar todos os produtos.
    - Implementado método cadastrarProduto com validações para nome duplicado, preço e quantidade.
    - Implementado método deletarProduto para remover produtos pelo nome.

---
+++ src/main/java/com/catalisa/ecomerce/zup/services/ProdutoService.java
@@ -0,0 +1,35 @@
+package com.catalisa.ecomerce.zup.services;
+
+
+import com.catalisa.ecomerce.zup.controllers.Produto;
+import com.catalisa.ecomerce.zup.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public List<Produto> listarProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    public void cadastrarProduto(Produto produto) {
+        if (produtoRepository.findByNome(produto.getNome()).isPresent()) {
+            throw new IllegalArgumentException("Já existe um produto com esse nome.");
+        }
+        if (produto.getPreco() <= 0 || produto.getQuantidade() < 0) {
+            throw new IllegalArgumentException("Preço ou quantidade inválidos.");
+        }
+        produtoRepository.save(produto);
+    }
+
+    public void deletarProduto(String nome) {
+        produtoRepository.deleteByNome(nome);
+    }
+}
