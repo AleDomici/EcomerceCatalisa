6a19bf259b9130dd057d6ad153d10e24a754c52d
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-02-04 15:50:11 -0300

    feat(compra): adicionar validações para os campos da classe Compra

--- src/main/java/com/catalisa/ecomerce/zup/model/Compra.java
+++ src/main/java/com/catalisa/ecomerce/zup/model/Compra.java
@@ -1,8 +1,16 @@
 package com.catalisa.ecomerce.zup.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public class Compra {
+    @NotNull(message = "O CPF do cliente é obrigatório.")
     private String cpfCliente;
+
+    @NotNull(message = "O ID do produto é obrigatório.")
     private Long idProduto;
+
+    @Positive(message = "A quantidade deve ser maior que 0.")
     private int quantidade;
 
     // Getters e Setters
